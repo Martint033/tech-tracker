430cfa47366fbb6e645a8025b9be6b2c5c1d7427
Tom <martint033@gmail.com>
2018-06-08 13:58:10 +0200

    struct ok

--- controllers/home.php
+++ controllers/home.php
@@ -5,5 +5,5 @@
     $twig = new Twig_Environment($loader, array(
       'cache' => false
     ));
-    $template = $twig->load('views/layout.html');
+    $template = $twig->load('layout.html');
     echo $template->render(array(''));
