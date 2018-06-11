f0ff071f7f2fbcf1a9f2d46fd85f466b4cdc5ead
tounssy <adnane.ben-ali@live.fr>
2018-06-11 16:29:50 +0200

    Add some css

--- assets/js/request.js
+++ assets/js/request.js
@@ -1,5 +1,3 @@
-
-
 fetch("https://api.github.com/search/users?q=location:besan%C3%A7on")
     .then( // on attend d'avoir complètement chargé le fichier, PUIS (then)on effectue la fonction 
         function (response){
