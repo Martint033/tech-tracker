7f75789f75b1bd2a26c958e14a19a0bcbad0541f
tounssy <adnane.ben-ali@live.fr>
2018-06-11 13:34:42 +0200

    Add responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,3 +38,8 @@ links.forEach(function (link) {
 map.addEventListener('mouseover', function () {
     activeArea()
 })
+
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
