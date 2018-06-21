2ad6b069939fc35da1ded1e6c4fb1f9b5e81d0d8
yannickACS <yannick.k@codeur.online>
2018-06-21 08:50:45 +0200

    ajout controlleurs page topFive

---
+++ controllers/top.php
@@ -0,0 +1,40 @@
+<?php
+require 'vendor/autoload.php';
+require "models/DataBase.php";
+require "models/DbManager.php";
+require "models/CommuneManager.php";
+require "models/RegionLanguageManager.php";
+require "models/CommuneLanguageManager.php";
+require "models/GeoElt.php";
+require "models/Region.php";
+require "models/Commune.php";
+   
+$loader = new Twig_Loader_Filesystem('views'); // Dossier contenant les templates
+$twig = new Twig_Environment($loader, array(
+  'cache' => false
+));
+$code_region = array('11' => 'Île-de-France',
+						'24' => 'Centre-Val de Loire',
+						'27' => 'Bourgogne-Franche-Comté',
+						'28' => 'Normandie',
+						'32' => 'Hauts-de-France',
+						'44' => 'Grand Est',
+						'52' => 'Pays de la Loire',
+						'53' => 'Bretagne',
+						'75' => 'Nouvelle-Aquitaine',
+						'76' => 'Occitanie',
+						'84' => 'Auvergne-Rhône-Alpes',
+						'93' => 'Provence-Alpes-Côte d\'Azur',
+                        '94' => 'Corse'); 
+                                  
+
+$allLanguage = ['php', 'javascript', 'python', 'java', 'ruby', 'c', 'cpp', 'csharp', 'assembly'];
+
+$townManager = new CommuneLanguageManager();
+$topVillesParRegion = array();
+$topTotalFrance = $townManager->topFive();
+foreach ($code_region as $keyReg => $valueReg){
+	$topVillesParRegion[] = array("code_region" => $valueReg, "villes" => $townManager->topFive((int)$keyReg));
+}
+$template = $twig->load('top.html');
+echo $template->render(array('topVillesParRegion' => $topVillesParRegion, 'topTotalFrance' => $topTotalFrance));
