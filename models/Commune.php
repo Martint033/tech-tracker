d40ce6d884c35254302bdca576f91418971467e0
yannickACS <yannick.k@codeur.online>
2018-06-21 08:45:45 +0200

    classe traitement communes

--- models/Commune.php
+++ models/Commune.php
@@ -5,16 +5,23 @@ class Commune extends GeoElt{
 	protected $nom_commune;
 	
 
-	public function __construct(int $id, string $nom_commune){
+	public function __construct(int $id, int $code_region, string $nom_commune){
 		$this->nom_commune = $nom_commune;
 		$this->id = $id;
+		$this->code_region = $code_region;
 	}
 	// setters
 	public function set_nom_commune(string $nom_commune){
 		$this->nom_commune = $nom_commune;
 	}
+	public function set_id(int $id){
+		$this->id = $id;
+	}
 	// getters	
 	public function get_nom_commune(){
 		return $this->nom_commune;
+	}
+	public function get_id(){
+		return $this->id;
 	}
 }
